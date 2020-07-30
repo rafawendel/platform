d91a9e75dc30e46caa66c1a5201de4e3cc337d11
Rafael Wendel <rafawendel2010@gmail.com>
2020-07-30 12:14:38 -0300

    removed Isabela

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -61,13 +61,6 @@ export default function Landing() {
       socialNetworks: [{ faIconClass: 'fad', bubbleColor: 'info' }]
     },
     {
-      src:
-        'https://lh3.googleusercontent.com/pw/ACtC-3czwYnvdvd9uK3St26yj-Ns8aFUEGrHztv33k_x3kawINWwV9F7H1dWAnW693rEPig52vkKQWXi7ZPz8UIs5-3Lg-9Hv8_-cn29WIQtBVjlnKimjBexPP5b06P8Au0Q5Xr1lsgIljO86rXdcyL8px3mJA=s2427-no?authuser=0',
-      name: 'Isabela Cristina',
-      title: 'Técnicas de estudo',
-      socialNetworks: [{ faIconClass: 'fad', bubbleColor: 'info' }]
-    },
-    {
       src: 'img/nogueira.jpeg',
       name: 'Vinicius Nogueira',
       title: 'Técnicas de apresentação',
