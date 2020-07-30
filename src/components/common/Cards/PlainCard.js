62461c9c599c28a413673d8dd4c7f943fc5c51af
Rafael Wendel <rafawendel2010@gmail.com>
2020-07-30 12:58:01 -0300

    fixed uppercase plain card

--- src/components/common/Cards/PlainCard.js
+++ src/components/common/Cards/PlainCard.js
@@ -4,8 +4,8 @@ export default function PlainCard({ title, details, textColor, faIconClass }) {
   return (
     <div className={`text-${textColor} w-full lg:w-3/12 px-4 text-center`}>
       <Bubble size="md" bubbleColor={textColor} faIconClass={faIconClass} />
-      <h5>{title}</h5>
-      <p className="mt-2 mb-4 font-normal uppercase">{details}</p>
+      <h5 className="uppercase">{title}</h5>
+      <p className="mt-2 mb-4 font-normal">{details}</p>
     </div>
   )
 }
