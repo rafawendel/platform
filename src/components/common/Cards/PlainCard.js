77b87c6745fc5b50716d88137bd7e11bbbd85669
Rafael Wendel <rafawendel2010@gmail.com>
2020-07-30 12:27:16 -0300

    added capitalize to plain card

--- src/components/common/Cards/PlainCard.js
+++ src/components/common/Cards/PlainCard.js
@@ -2,7 +2,7 @@ import Bubble from '../Bubbles/Bubble'
 
 export default function PlainCard({ title, details, textColor, faIconClass }) {
   return (
-    <div className={`text-${textColor} w-full lg:w-3/12 px-4 text-center`}>
+    <div className={`text-${textColor} capitalize w-full lg:w-3/12 px-4 text-center`}>
       <Bubble size="md" bubbleColor={textColor} faIconClass={faIconClass} />
       <h5>{title}</h5>
       <p className="mt-2 mb-4 font-normal">{details}</p>
