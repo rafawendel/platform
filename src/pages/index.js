ad3cb5f1eb4aa7d91fb65f621fc320d755f364f6
Rafael Wendel <rafawendel2010@gmail.com>
2020-08-13 01:34:00 -0300

    updated index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,7 @@
 import { useRouter } from 'next/router'
-// import Ektelesi from './ektelesi'
-// import PlainModal from '../components/common/Modals/PlainModal'
 
 export default function Home() {
   const router = useRouter()
   router.push('/ektelesi')
-  return (
-    <div />
-    // {/* <>
-    //   {/* <PlainModal
-    //     buttons={['Ir até lá', 'Mais tarde']}
-    //     redirect={() => {
-    //       router.push('/ektelesi')
-    //     }}
-    //   >
-    //     <p>O evento já começou! Entre já na página inicial!</p>
-    //   </PlainModal> */}
-
-    // </div> */}
-  )
+  return <div />
 }
