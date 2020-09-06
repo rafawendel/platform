0cd8ba7274dd974a743f5f788ca13a4460e878f0
Rafael Wendel <rafawendel2010@gmail.com>
2020-09-06 13:39:30 -0300

    fixed server side id

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -5,13 +5,13 @@ const DB_URL = `https://script.google.com/macros/s/${process.env.DB_ID}/exec`
 export default async (req, res) => {
   if (req.method === 'POST') {
     try {
-      const { formId, ...payload } = req.body
+      const { formId, id, ...payload } = req.body
       const schema = getValidationSchema(getFormSchemaByName(formId))
 
       const isValid = await schema.isValid(payload)
       if (!isValid) throw new Error('Invalid data format')
 
-      const dbRes = await axios.post(DB_URL, payload)
+      const dbRes = await axios.post(DB_URL, { ...payload, id })
 
       if (dbRes.error) throw new Error(dbRes.error)
       if (dbRes.data.error) {
