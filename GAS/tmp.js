0ffcfc3e57807df1788f0ecff21326afe165afb2
Rafael Wendel <rafawendel2010@gmail.com>
2020-09-16 20:46:14 -0300

    moved server to another repo

--- GAS/tmp.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  const content = ContentService.createTextOutput('')
-  const ui  = SpreadsheetApp.getUi
-  ui.showSidebar(content)
-}
-
-function onOpen() {
-  myFunction()
-}
--- src/pages/api/groups.js
+++ src/pages/api/groups.js
@@ -1,13 +1,18 @@
 import fs from 'fs'
+import path from 'path'
 
 export default async (req, res) => {
   if (req.method === 'GET') {
     try {
       const groups = await new Promise((resolve, reject) => {
-        fs.readFile('./groups.json', null, (err, data) => {
-          if (err) reject(err)
-          resolve(data)
-        })
+        fs.readFile(
+          path.resolve(process.cwd(), 'src/pages/api', './groups.json'),
+          null,
+          (err, data) => {
+            if (err) reject(err)
+            resolve(data)
+          }
+        )
       }).then(g => JSON.parse(g))
       res.status(200).json({ groups })
     } catch (err) {
