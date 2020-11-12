9dd58242287f9286ff9e81080a70beeabbdcb244
Rafael Wendel <rafawendel2010@gmail.com>
2020-11-12 20:49:29 -0300

    privatized pages, added research form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/motiro')
+  }, [])
+
   return <p className="w-screen text-center text-4xl h-screen align-middle">Em breve</p>
 }
