882fbf07da7ef7b9ddad59c01923b5094fd44f5f
Rafael Wendel <rafawendel2010@gmail.com>
2021-02-01 10:35:49 -0300

    Fixed groups API

--- src/pages/api/groups.js
+++ src/pages/api/groups.js
@@ -1,50 +1,74 @@
 import axios from 'axios'
+import { isNumber, pick } from 'lodash'
 
 const DB_URL = `https://script.google.com/macros/s/${process.env.DB_ID}/exec`
 
-let GROUPS = []
+let cachedGroups = []
+
+async function updateCachedGroups() {
+  const dbRequestConfig = {
+    params: {
+      operation: 'groups'
+    }
+  }
+
+  if (!cachedGroups[0]) {
+    const {
+      data: { groups: dbGroups }
+    } = await axios.get(DB_URL, dbRequestConfig)
+
+    const groups = dbGroups
+      .filter(g => isNumber(g.id))
+      .map(g =>
+        pick(g, [
+          'id',
+          'vacancies',
+          'openVacancies',
+          'leaders',
+          'title',
+          'specialty',
+          'description',
+          'weekDay',
+          'startsAt',
+          'endsAt',
+          'lang',
+          'preferenceByYear',
+          'preferenceByCollege'
+        ])
+      )
+
+    cachedGroups = [...groups]
+  } else {
+    // makes an asynchronous update
+    axios
+      .get(DB_URL, dbRequestConfig)
+      .catch(console.error)
+      .then(({ data: { groups: dbGroups } }) => {
+        console.log('Updating groups asynchronously...')
+        cachedGroups = [...dbGroups]
+      })
+  }
+}
 export default async (req, res) => {
   if (req.method === 'GET') {
     const {
       query: { gid }
     } = req
-    // const schema = Yup.array().of(Yup.object(GroupType))
-    // const areCachedGroupsValid = await schema.isValid(GROUPS)
 
-    const dbRequestConfig = {
-      params: {
-        operation: 'groups'
-      }
-    }
+    await updateCachedGroups()
+      .then(() => {
+        const groupIds = cachedGroups.map(g => String(g.id))
+        const groups =
+          gid && groupIds.includes(gid)
+            ? cachedGroups.filter(g => String(g.id) === gid)
+            : cachedGroups
 
-    try {
-      if (!GROUPS[0]) {
-        const {
-          data: { groups: dbGroups }
-        } = await axios.get(DB_URL, dbRequestConfig)
-
-        // await schema.isValid(groups).then(isValid => {
-        //   if (!isValid) throw new Error('Invalid data format')
-        // })
-
-        GROUPS = [...dbGroups]
-      } else {
-        // makes an asynchronous update
-        axios
-          .get(DB_URL, dbRequestConfig)
-          .catch(err => console.error(err))
-          .then(({ data: { groups: dbGroups } }) => {
-            console.log('Updating groups asynchronously...')
-            GROUPS = [...dbGroups]
-          })
-      }
-      const groupIds = GROUPS.map(g => String(g.id))
-      const groups =
-        gid && groupIds.includes(gid) ? GROUPS.filter(g => String(g.id) === gid) : GROUPS
-      res.status(200).json({ groups })
-    } catch (err) {
-      res.status(500).json({ error: err.message || err, groups: GROUPS })
-    }
+        res.status(200).json({ groups })
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).json({ error: err.message || err, groups: cachedGroups })
+      })
   } else {
     res.status(400).json({ message: `there is nothing to ${req.method.toLowerCase()} here` })
   }
