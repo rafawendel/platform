f7f1475a8aacf923606f2bbef8dfc4381835272b
Rafael Wendel <44540213+rafawendel@users.noreply.github.com>
2024-05-08 14:05:29 +0000

    fix login form

--- src/pages/eventos/login.js
+++ src/pages/eventos/login.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import { useContext, useEffect } from 'react'
-import LoginForm from '../../components/Forms/Login'
+import LoginForm from '../../components/forms/UserForm'
 import { AuthContext } from '../../context/auth'
 
 export default function LoginPage({ setLoading }) {
